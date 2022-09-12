ca19f095c02e31fd3856c73f3ab48cea05b70dfd
TxK-Code <80899114+TxK-Code@users.noreply.github.com>
2022-09-12 17:58:34 +0200

    1.01

--- wcs-quest/front/src/Containers/Index.js
+++ wcs-quest/front/src/Containers/Index.js
@@ -127,17 +127,18 @@ export default function Index() {
 
   return (
     <>
-      <header>
-        <h1>
+      <header className="header">
+        <h1 className="header__title">
           <img
             src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png"
             alt="Wild Code School logo"
+            className="header__img"
           />
           Les Argonautes
         </h1>
       </header>
 
-      <main>
+      <main className="main">
         <h2>Ajouter un(e) Argonaute</h2>
         <form
           className="new-member-form"
